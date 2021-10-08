8a785758186795d04cdfd029762e883c17bf7f47
Girish Jagtap <Girish.Jagtap@prolifics.com>
2021-10-08 18:01:05 +0530

    Commit after 6.4.b partially

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace ProlificsProjectManager
             }
             CommonInterface commonInterface = new CommonInterface();
             commonInterface.StartProgram();
+           
         }
     }
 }
